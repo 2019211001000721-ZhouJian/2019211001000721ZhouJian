3095730b89e0fe585004b2669bcd67bbbb7069cd
2019211001000721ZhouJian <2731594431@qq.com>
2021-04-18 15:10:09 +0800

    HW week7 Commit

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,32 +1,71 @@
 package com.dao;
 
 import com.model.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao implements IUserDao{
     @Override
     public boolean saveUser(Connection con, User user) throws SQLException {
-        return false;
+        String sql = "insert into usertable values(?,?,?,?,?)";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,user.getUsername());
+        st.setString(2,user.getPassword());
+        st.setString(3,user.getEmail());
+        st.setString(4,user.getGender());
+        st.setString(5,user.getBirthdate());
+        int rs = st.executeUpdate();
+        if(rs == 0){
+            return false;
+        }else {
+            return true;
+        }
     }
 
     @Override
     public int deleteUser(Connection con, User user) throws SQLException {
-        return 0;
+        String sql = "delete from usertable where id = ?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1,user.getId());
+        int rs = st.executeUpdate();
+        return rs;
     }
 
     @Override
     public int updateUser(Connection con, User user) throws SQLException {
-        return 0;
+        String sql = "update usertable set username=?,password=?,email=?,gender=?,birthDate=? where id = ?";
+        PreparedStatement st=con.prepareStatement(sql);
+        st.setString(1,user.getUsername());
+        st.setString(2,user.getPassword());
+        st.setString(3,user.getEmail());
+        st.setString(4,user.getGender());
+        st.setString(5,user.getBirthdate());
+        st.setInt(6,user.getId());
+        int rs = st.executeUpdate();
+        return rs;
     }
 
     @Override
     public User findById(Connection con, Integer id) throws SQLException {
-        return null;
+        String sql = "select * from usertable where id=?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1,id);
+        ResultSet rs = st.executeQuery();
+        User user = null;
+        if(rs.next()){
+            user = new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+        }
+        return user;
     }
 
     @Override
@@ -52,31 +91,119 @@ public class UserDao implements IUserDao{
 
     @Override
     public List<User> findByUsername(Connection con, String username) throws SQLException {
-        return null;
+        String sql = "select * from usertable where username=?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,username);
+        ResultSet rs = st.executeQuery();
+        User user = new User();
+        List<User> list = new ArrayList<User>();
+        if(rs.next()){
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByPassword(Connection con, String password) throws SQLException {
-        return null;
+        String sql = "select * from usertable where password=?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,password);
+        ResultSet rs = st.executeQuery();
+        User user = new User();
+        List<User> list = new ArrayList<User>();
+        if(rs.next()){
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByEmail(Connection con, String email) throws SQLException {
-        return null;
+        String sql = "select * from usertable where email=?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,email);
+        ResultSet rs = st.executeQuery();
+        User user = new User();
+        List<User> list = new ArrayList<User>();
+        if(rs.next()){
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByGender(Connection con, String gender) throws SQLException {
-        return null;
+        String sql = "select * from usertable where gender=?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,gender);
+        ResultSet rs = st.executeQuery();
+        User user = new User();
+        List<User> list = new ArrayList<User>();
+        if(rs.next()){
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findByBirthdate(Connection con, String birthdate) throws SQLException {
-        return null;
+        String sql = "select * from usertable where birthDate=?";
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setString(1,birthdate);
+        ResultSet rs = st.executeQuery();
+        User user = new User();
+        List<User> list = new ArrayList<User>();
+        if(rs.next()){
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+            list.add(user);
+        }
+        return list;
     }
 
     @Override
     public List<User> findAllUser(Connection con) throws SQLException {
-        return null;
+        String sql = "select * from usertable";
+        ResultSet rs = con.createStatement().executeQuery(sql);
+        User user = new User();
+        List<User> list = new ArrayList<User>();
+        if(rs.next()){
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmail(rs.getString("email"));
+            user.setGender(rs.getString("gender"));
+            user.setBirthdate(rs.getString("birthDate"));
+            list.add(user);
+        }
+        return list;
     }
 }
---
+++ target/ZhouJian2019211001000721-1.0-SNAPSHOT/WEB-INF/views/userInfo.jsp
@@ -0,0 +1,28 @@
+<%@ page import="com.model.User" %><%--
+  Created by IntelliJ IDEA.
+  User: 32203
+  Date: 2021/4/11
+  Time: 17:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@include file="header.jsp"%>
+<h1>User Info</h1>
+<%
+    User user=(User)request.getAttribute("user");
+%>
+<table>
+    <tr>
+        <td>username:</td><td><%=user.getUsername()%></td>
+    </tr><tr>
+        <td>password:</td><td><%=user.getPassword()%></td>
+    </tr><tr>
+        <td>email:</td><td><%=user.getEmail()%></td>
+    </tr><tr>
+        <td>gender:</td><td><%=user.getGender()%></td>
+    </tr><tr>
+        <td>birthdate:</td><td><%=user.getBirthdate()%></td>
+    </tr>
+</table>
+
+<%@include file="footer.jsp"%>
